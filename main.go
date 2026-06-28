947b8b962eb551617e8d42a24f33ced7498c94f7
Kayra <kayra19206@gmail.com>
2026-06-28 20:00:44 +1000

    Added a custom handler

--- main.go
+++ main.go
@@ -8,6 +8,19 @@ import (
 func main() {
 	serverMux := http.NewServeMux()
 
+	h1 := func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			log.Fatal("error writing body")
+		}
+	}
+
+	serverMux.HandleFunc("/healthz", h1)
+
+	serverMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
