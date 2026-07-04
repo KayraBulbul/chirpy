80ac0110bdc15a1b799faf362d6fbfe256b902ea
Kayra <kayra19206@gmail.com>
2026-07-04 13:20:00 +1000

    Moved apiConfig struct and methods to separate file

---
+++ config.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+
+	"github.com/KayraBulbul/chirpy/internal/database"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getHits() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		_, err := w.Write([]byte(fmt.Sprintf("<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n</html>", cfg.fileserverHits.Load())))
+		if err != nil {
+			log.Fatal("error writing hits body")
+		}
+	})
+}
+
+func (cfg *apiConfig) resetHits() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Store(0)
+	})
+}
--- main.go
+++ main.go
@@ -3,45 +3,15 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-	"sync/atomic"
 
 	"github.com/KayraBulbul/chirpy/internal/database"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
-
-type apiConfig struct {
-	fileserverHits atomic.Int32
-	dbQueries      *database.Queries
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func (cfg *apiConfig) getHits() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		_, err := w.Write([]byte(fmt.Sprintf("<html>\n<body>\n<h1>Welcome, Chirpy Admin</h1>\n<p>Chirpy has been visited %d times!</p>\n</body>\n</html>", cfg.fileserverHits.Load())))
-		if err != nil {
-			log.Fatal("error writing hits body")
-		}
-	})
-}
-
-func (cfg *apiConfig) resetHits() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Store(0)
-	})
-}
 
 func respondWithError(w http.ResponseWriter, code int, msg string) {
 	type returnVal struct {
@@ -156,6 +126,7 @@ func main() {
 	serverMux.Handle("GET /admin/metrics", apiCfg.getHits())
 	serverMux.Handle("POST /admin/reset", apiCfg.resetHits())
 	serverMux.Handle("POST /api/validate_chirp", validateChirp())
+	serverMux.Handle("POST /api/users")
 
 	server := http.Server{
 		Addr:    ":8080",
